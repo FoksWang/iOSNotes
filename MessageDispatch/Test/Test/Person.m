e1aabc8aadce52f02df9eadcbf7504dbe13fe5c5
Hui Wang <foks.wang@gmail.com>
2019-04-08 07:48:03 +0200

    Add 01 objc_msgsend

---
+++ MessageDispatch/Test/Test/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Test
+//
+//  Created by Hui Wang on 2019-04-06.
+//  Copyright © 2019 Hui Wang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
